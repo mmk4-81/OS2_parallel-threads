fc7ef5fded60063c30e44d756d59b634afe0bcc3
mmk <mmkkarbalaei@gmail.com>
2024-02-07 23:53:11 +0330

    Perform calculations

--- main.cpp
+++ main.cpp
@@ -102,7 +102,54 @@ int main()
 
 DWORD WINAPI F(LPVOID param)
 {
-    MyParam* input = static_cast<MyParam*>(param);
-    double* localResult = input->localResult;
-    DWORD threadID = input->threadID; 
+    MyParam *input = static_cast<MyParam *>(param);
+    double *localResult = input->localResult;
+    DWORD threadID = input->threadID;
+
+    srand(time(NULL) + threadID);
+    auto start_time = high_resolution_clock::now();
+
+    double diff = abs(localResult[DataSize + 1] - 0.4 * totalLands) + abs(localResult[DataSize + 2] - 0.4 * totalLands) + abs(localResult[DataSize + 3] - 0.2 * totalLands);
+
+    while (true)
+    {
+
+        double *distribution = new double[DataSize];
+        int sum[3] = {0};
+
+        for (int i = 0; i < DataSize; i++)
+        {
+            int randomindex = rand() % 3;
+            sum[randomindex] += Lands[i];
+            distribution[i] = randomindex;
+        }
+
+        double newdiff = abs(sum[0] - 0.4 * totalLands) + abs(sum[1] - 0.4 * totalLands) + abs(sum[2] - 0.2 * totalLands);
+
+        if (newdiff < diff)
+        {
+            diff = newdiff;
+
+            for (int i = 0; i < DataSize; i++)
+            {
+                localResult[i + 1] = distribution[i];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                localResult[DataSize + i + 1] = sum[i];
+            }
+        }
+
+        delete[] distribution;
+
+        auto end_time = high_resolution_clock::now();
+        auto elapsed_time = duration_cast<duration<double>>(end_time - start_time).count();
+
+        if (elapsed_time >= runtime)
+        {
+            break;
+        }
+    }
+
+    return 0;
 }
